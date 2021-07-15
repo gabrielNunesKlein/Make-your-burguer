741de96a9f809f7f50882b3365e61a22f0c5a065
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-15 18:16:37 -0300

    Criando o formulario

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu burguer</h1>
-    <p>Formulario</p>
+    <BurguerForm />
   </div>
 </template>
 
 <script>
 import Banner from '../components/Banner.vue';
+import BurguerForm from '../components/BurguerForm.vue';
 export default {
   name: 'Home',
   components: {
-    Banner
+    Banner,
+    BurguerForm
   }
 }
 </script>
