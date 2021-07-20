16b21ec521aa93a1c9f4999347c0277a40f153a7
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-20 15:09:26 -0300

    Resgatando pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,16 @@
             </div>
         </div>
         <div id="burguer-table-rows">
-            <div class="burguer-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão tigre</div>
-                <div>Maminha</div>
+            <div class="burguer-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{ opcional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
@@ -29,37 +30,33 @@
                     <button class="delete-btn">Cancelar</button>
                 </div>
             </div>
-        
-        
-        </div>
-                <div id="burguer-table-rows">
-            <div class="burguer-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão tigre</div>
-                <div>Maminha</div>
-                <div>
-                    <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
-                    </ul>
-                </div>
-                <div>
-                    <select name="status" class="status">
-                        <option value="">Selecione</option>
-                    </select>
-                    <button class="delete-btn">Cancelar</button>
-                </div>
-            </div>
-        
-        
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data(){
+        return {
+            burgers: null,
+            buger_id: null,
+            status: []
+        }
+    },
+    methods:{
+        async getPedidos(){
+            const req = await fetch("http://localhost:3000/burgers");
+
+            const data = await req.json();
+            this.burgers = data;
+
+            console.log(this.burgers);
+        }
+    },
+    mounted(){
+        this.getPedidos();
+    }
 }
 </script>
 
